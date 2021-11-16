8386cda784669dff9a8de285c2cc312d33058433
Pchemelil <85558913+Pchemelil@users.noreply.github.com>
2021-11-16 21:50:21 +0300

    Add files via upload

---
+++ forLoop(12-28).cpp
@@ -0,0 +1,12 @@
+//137339 Peter Chemelil
+//for loop exercise 1
+
+#include <iostream>
+using namespace std;
+
+int main(){
+	int number;
+	
+	for(number=12; number<=28; number= number + 2)
+	cout<< "\n"<<number;
+}
